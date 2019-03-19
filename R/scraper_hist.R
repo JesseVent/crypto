9c33fbffbcbe1f5a7517b3d75b6a53e6ffac3992
Diffform <sstoeckl@gmx.de>
2019-03-19 14:27:31 +0100

    Create function to create history coin lists as can be found on CMC, e.g. https://coinmarketcap.com/historical/20150505/

---
+++ R/scraper_hist.R
@@ -0,0 +1,58 @@
+#' Historical table scraper
+#'
+#' This web scrapes the historic price tables from CoinMarketCap
+#' and provides back a dataframe for the coin provided as an input.
+#' This function is a dependency of getCoins and is used
+#' as part of a loop to retrieve all crypto currencies.
+#'
+#' @param attributes URL generated from \code{listCoins()}
+#' @param slug Unique identifier required for merging
+#' @param sleep Duration to sleep to resolve rate limiter
+#'
+#' @return Raw OHLC market data in a dataframe:
+#'   \item{slug}{Coin url slug}
+#'   \item{symbol}{Coin symbol}
+#'   \item{name}{Coin name}
+#'   \item{date}{Market date}
+#'   \item{open}{Market open}
+#'   \item{high}{Market high}
+#'   \item{low}{Market low}
+#'   \item{close}{Market close}
+#'   \item{volume}{Volume 24 hours}
+#'   \item{market}{USD Market cap}
+#'
+#' @importFrom dplyr "%>%" "mutate" "select"
+#' @importFrom tibble "as_tibble"
+#' @importFrom tidyr "separate"
+#' @importFrom rvest "html_nodes" "html_table"
+#' @importFrom xml2 "read_html"
+#' @importFrom curl "new_handle"
+#'
+scraper_hist <- function(attributes, sleep = NULL) {
+  .            <- "."
+  history_url  <- as.character(attributes)
+  if (!is.null(sleep)) Sys.sleep(sleep)
+
+  page <- tryCatch(
+    xml2::read_html(history_url,
+                    handle = curl::new_handle("useragent" = "Mozilla/5.0")),
+    error = function(e) e)
+
+  if (inherits(page, "error")) {
+    closeAllConnections()
+    message("\n")
+    message(cli::cat_bullet("Rate limit hit. Sleeping for 60 seconds.", bullet = "warning", bullet_col = "red"), appendLF = TRUE)
+    Sys.sleep(65)
+    page <- xml2::read_html(history_url,
+                            handle = curl::new_handle("useragent" = "Mozilla/5.0"))
+  }
+
+  table   <- rvest::html_nodes(page, css = "table") %>% .[1] %>%
+    rvest::html_table(fill = TRUE) %>%
+    replace(!nzchar(.), NA)
+
+  scraper <- table[[1]][,2:3] %>% tibble::as_tibble() %>%
+    tidyr::separate(Name,sep = "\n",into=c("symbol","name")) %>% mutate(slug=sub("[^[:alnum:]]","-",sub("[[:punct:]]","-",tolower(name)))) %>%
+    dplyr::select(-Symbol)
+  return(scraper)
+}
